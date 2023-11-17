d324f1b94febea7c3e48ec94a9dca6a32b5ca36c
oonixx <imrane.achaari@yahoo.com>
2023-11-16 18:05:03 -0800

    the 0 task

---
+++ 0-bubble_sort.c
@@ -0,0 +1,46 @@
+#include "sortalgo.h"
+
+/**
+ * swap_ints - Swap two integers in the array.
+ * @a: First integer to swap.
+ * @b: Second integer to swap.
+ */
+void swap_ints(int *a, int *b)
+{
+	int tmp;
+
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
+
+/**
+ * bubble_sort - Sort the array of integers in the ascending order.
+ * @array: Array of integers to sort.
+ * @size: Size of the array.
+ *
+ * Description: Prints array after each swap.
+ */
+void bubble_sort(int *array, size_t size)
+{
+	size_t index, len = size;
+	bool bubbly = false;
+
+	if (array == NULL || size < 2)
+		return;
+
+	while (bubbly == false)
+	{
+		bubbly = true;
+		for (index = 0; index < len - 1; index++)
+		{
+			if (array[index] > array[index + 1])
+			{
+				swap_ints(array + index, array + index + 1);
+				print_array(array, size);
+				bubbly = false;
+			}
+		}
+		len--;
+	}
+}
---
+++ sortalgo.h
@@ -0,0 +1,54 @@
+#ifndef SORTALGO_H
+#define SORTALGO_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/* Comparison the direction macros for the bitonic sort */
+#define UP 0
+#define DOWN 1
+
+/**
+ * enum bool - Enumeration of the Boolean values.
+ * @false: 0.
+ * @true: 1.
+ */
+typedef enum bool
+{
+	false = 0,
+	true
+} bool;
+
+/**
+ * struct listint_s - The doubly linked list node
+ *
+ * @n: The integer stored in node
+ * @prev: The pointer to the previous element of list
+ * @next: The pointer to the next element of list
+ */
+typedef struct listint_s
+{
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
+} listint_t;
+
+/* Printing the helper functions */
+void print_array(const int *array, size_t size);
+void print_list(const listint_t *list);
+
+/* Sorting the algoritms */
+void counting_sort(int *array, size_t size);
+void merge_sort(int *array, size_t size);
+void heap_sort(int *array, size_t size);
+void radix_sort(int *array, size_t size);
+void bitonic_sort(int *array, size_t size);
+void quick_sort_hoare(int *array, size_t size);
+void bubble_sort(int *array, size_t size);
+void insertion_sort_list(listint_t **list);
+void selection_sort(int *array, size_t size);
+void quick_sort(int *array, size_t size);
+void shell_sort(int *array, size_t size);
+void cocktail_sort_list(listint_t **list);
+
+#endif /* SORTALGO_H */
